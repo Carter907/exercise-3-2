7120a1f79a36c22526508d24f99d81749be84453
Carter <102479896+Carter907@users.noreply.github.com>
2024-06-07 03:30:50 +0000

    Added Branching

--- main.cs
+++ main.cs
@@ -1,7 +1,46 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter the windspeed of the Hurricane");
+        double speed = Convert.ToDouble(Console.ReadLine());
+
+        if (speed >= 157)
+        {
+            Console.WriteLine("Category Five Hurricane");
+
+
+        }
+        else if (speed >= 130)
+        {
+            Console.WriteLine("Category Four Hurricane");
+
+
+        }
+        else if (speed >= 111)
+        {
+            Console.WriteLine("Category Three Hurricane");
+
+        }
+        else if (speed >= 96)
+        {
+
+            Console.WriteLine("Category Two Hurricane");
+
+        }
+        else if (speed >= 74)
+        {
+
+            Console.WriteLine("Category One Hurricane");
+
+        }
+        else
+        {
+
+            Console.WriteLine("Not a Hurricane");
+        }
+    }
+}
 }
